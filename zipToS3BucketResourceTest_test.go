f5edde4b471f2ffcb19e68def265320fb4b81326
Matt Weagle <mweagle@gmail.com>
2016-04-22 07:14:05 -0700

    ZIP Archive to S3 Bucket CustomResource

---
+++ zipToS3BucketResourceTest_test.go
@@ -0,0 +1,35 @@
+package cloudformationresources
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+	gocf "github.com/crewjam/go-cloudformation"
+)
+
+func TestUnzip(t *testing.T) {
+	resUnzip := gocf.NewResourceByType(ZipToS3Bucket)
+	zipResource := resUnzip.(*ZipToS3BucketResource)
+	zipResource.DestBucket = gocf.String(os.Getenv("TEST_DEST_S3_BUCKET"))
+	zipResource.SrcBucket = gocf.String(os.Getenv("TEST_SRC_S3_BUCKET"))
+	zipResource.SrcKeyName = gocf.String(os.Getenv("TEST_SRC_S3_KEY"))
+	zipResource.Manifest = map[string]interface{}{
+		"Some": "Data",
+	}
+	// Put it
+	logger := logrus.New()
+	awsSession := awsSession(logger)
+	createOutputs, createError := zipResource.create(awsSession, logger)
+	if nil != createError {
+		t.Errorf("Failed to create Unzip resource: %s", createError)
+	}
+	t.Logf("TestUnzip outputs: %s", createOutputs)
+
+	deleteOutputs, deleteError := zipResource.delete(awsSession, logger)
+	if nil != deleteError {
+		t.Errorf("Failed to create Unzip resource: %s", createError)
+	}
+	t.Logf("TestUnzip outputs: %s", deleteOutputs)
+
+}
