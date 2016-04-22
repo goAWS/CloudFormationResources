23bbf2eda92b2e2e0ff80a4ef161b05d56fbe41a
Matt Weagle <mweagle@gmail.com>
2016-04-22 07:14:35 -0700

    Rename to more go-friendly filenames

--- cloudFormationResources.go
+++ cloudFormationResources.go
@@ -82,6 +82,8 @@ var (
 	HelloWorld = cloudFormationResourceType("HelloWorldResource")
 	// S3LambdaEventSource is the typename for S3LambdaEventSourceResource
 	S3LambdaEventSource = cloudFormationResourceType("S3LambdaEventSourceResource")
+	// ZipToS3Bucket is the typename for ZipToS3Bucket
+	ZipToS3Bucket = cloudFormationResourceType("ZipToS3BucketResource")
 )
 
 func sendCloudFormationResponse(customResourceRequest *CustomResourceRequest,
@@ -204,6 +206,16 @@ func customCommandForTypeName(resourceTypeName string, properties *[]byte) (inte
 		customCommand = &command
 	case S3LambdaEventSource:
 		command := S3LambdaEventSourceResource{
+			GoAWSCustomResource: GoAWSCustomResource{
+				GoAWSType: resourceTypeName,
+			},
+		}
+		if nil != properties {
+			unmarshalError = json.Unmarshal([]byte(string(*properties)), &command)
+		}
+		customCommand = &command
+	case ZipToS3Bucket:
+		command := ZipToS3BucketResource{
 			GoAWSCustomResource: GoAWSCustomResource{
 				GoAWSType: resourceTypeName,
 			},
